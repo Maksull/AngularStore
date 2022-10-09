dd1205304138dd5d35baf5bf4c898f4baf7664ff
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2022-10-09 16:22:11 +0300

    Admin module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
+import { AuthGuard } from "../guards/authGuard.guard";
 import { AdminComponent } from "./admin.component";
 import { AuthComponent } from "./auth.component";
+import { CategoryEditorComponent } from "./category/categoryEditor.component";
+import { CategoryTableComponent } from "./category/categoryTable.component";
 import { MaterialFeatures } from "./material.module";
+import { ProductEditorComponent } from "./product/productEditor.component";
+import { ProductTableComponent } from "./product/productTable.component";
+import { SupplierEditorComponent } from "./supplier/supplierEditor.component";
+import { SupplierTableComponent } from "./supplier/supplierTable.component";
 
 @NgModule({
     imports: [MaterialFeatures, RouterModule, FormsModule, BrowserModule],
-    declarations: [AdminComponent, AuthComponent],
-    providers: []
+    declarations: [AdminComponent, AuthComponent, 
+        ProductTableComponent, ProductEditorComponent, 
+        CategoryTableComponent, CategoryEditorComponent,
+        SupplierTableComponent, SupplierEditorComponent],
+    providers: [AuthGuard]
 })
 export class AdminModule { }
---
+++ src/app/admin/material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatInputModule } from "@angular/material/input";
+import { MatOptionModule } from "@angular/material/core";
+import { MatSelectModule } from "@angular/material/select";
+
+const features: any[] = [MatToolbarModule, MatIconModule, MatSidenavModule, MatButtonModule, MatDividerModule, MatTableModule, MatPaginatorModule,
+    MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule];
+
+@NgModule({
+    imports: [features],
+    exports: [features]
+})
+export class MaterialFeatures { }
